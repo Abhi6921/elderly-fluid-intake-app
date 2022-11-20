173e11cc34b58d64adf2b4a066ea532e70171852
Abhishek <abhishek.narvekar80@gmail.com>
2022-11-20 17:25:27 +0100

    login funtionality works properly

--- app/src/main/java/nl/narvekar/abhishek/omring_fluid_intake_app/api/DrinkAuthApi.kt
+++ app/src/main/java/nl/narvekar/abhishek/omring_fluid_intake_app/api/DrinkAuthApi.kt
@@ -7,10 +7,12 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.Headers
 import retrofit2.http.POST
 
 interface DrinkAuthApi {
 
+    @Headers("Content-Type:application/json")
     @POST("api/v1/login")
     fun loginUser(@Body login: Login) : Call<LoginResponse>
 
